4379302137deb636741f10c3b135664310a5293a
monjjjjj <monjjjj526@gmail.com>
2023-04-20 11:49:53 +0800

    Init

--- src/RentalSystem/RentalRelation.java
+++ src/RentalSystem/RentalRelation.java
@@ -1,6 +1,11 @@
 package RentalSystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RentalRelation {
+	private List<Customer> customers = new ArrayList<Customer>();
+	
 	private RentalRelation() {}
 	
 
